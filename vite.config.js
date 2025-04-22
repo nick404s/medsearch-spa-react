5d04f357f300bdcbb3781539013ead9fe8b6e373
nick404s <nickskryabin@gmail.com>
2025-04-22 08:56:21 -0700

    added base name to router

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 // "homepage": "https://nick404s.github.io/medsearch-spa-react/",
 export default defineConfig({
+  base: "/medsearch-spa-react/",
   plugins: [react()],
-  base: "/medsearch-spa-react",
 });
