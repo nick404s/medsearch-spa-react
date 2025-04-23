8bdc213860067ea5eacaa519fe026b45d39ed522
nick404s <nickskryabin@gmail.com>
2025-04-22 17:55:47 -0700

    enhanced navigation button, edited readme

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ## MedSearch is a Single Page React App
 
+[Link to the App]{https://nick404s.github.io/medsearch-spa-react/}
+
 ### Allows a user search for medications by ingredient or condition
 
 #### Using OpenFDA data as a source
--- src/components/NavigationButton.jsx
+++ src/components/NavigationButton.jsx
@@ -1,16 +1,34 @@
-import { Link, ScrollRestoration, useNavigate } from "react-router-dom";
-import { HiChevronLeft } from "react-icons/hi";
-const NavigationButton = ({ route }) => {
+import { useNavigate } from "react-router-dom";
+import { HiChevronLeft, HiHome } from "react-icons/hi";
+const NavigationButton = ({ route, isBack = true, text }) => {
   const navigate = useNavigate();
+
+  if (isBack) {
+    return (
+      <div className="navigation">
+        <button
+          className="btn-nav"
+          onClick={() => {
+            navigate(-1);
+          }}
+        >
+          <HiChevronLeft />
+          {text || "Back"}
+        </button>
+      </div>
+    );
+  }
+
   return (
     <div className="navigation">
       <button
         className="btn-nav"
         onClick={() => {
-          navigate(-1);
+          navigate(route);
         }}
       >
-        <HiChevronLeft /> Back
+        <HiHome />
+        {text || "Home"}
       </button>
     </div>
   );
